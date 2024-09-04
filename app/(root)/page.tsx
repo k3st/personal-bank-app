e7a312fdfd58684fd86482d6654fd84742165744
kent mercado <merken.work@gmail.com>
2024-09-04 21:48:10 +0800

    add header box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import HeaderBox from "../../components/HeaderBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
